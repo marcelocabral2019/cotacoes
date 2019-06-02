a83092a0a8d0b1e0764644aff77fdc712a86fb29
marcelocabral2019 <marcelo.jccabral@gmail.com>
2019-06-02 12:32:22 -0300

    acertos

--- src/about.js
+++ src/about.js
@@ -1,7 +1,7 @@
 const pessoas = []
 
-pessoas.push({nome: 'Cabral',tel: '21 99999-9999'})
-pessoas.push({nome: 'Angelica',tel: '21 2222-2222'})
+pessoas.push({nome: 'Alberto',tel: '21 99999-9999'})
+pessoas.push({nome: 'Caverna',tel: '21 2222-2222'})
 pessoas.push({nome: 'Julio',tel: '21 4557-3232'})
 pessoas.push({nome: 'Carla',tel: '21 3355-1665'})
 pessoas.push({nome: 'Fernando',tel: '21 3355-1665'})
